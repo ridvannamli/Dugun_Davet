8936dc468f4f418ee729032477c90ab585b764ac
ridvannamli <ridvannamli@gmail.com>
2022-10-29 21:01:17 +0300

    Project folders added.

---
+++ Dugun_Davet/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using Dugun_Davet.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Dugun_Davet.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ViewResult CevapFormu()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult CevapFormu(Cevap KatilimciCevabi)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.CevapEkle(KatilimciCevabi);
+                return View("Tesekkurler", KatilimciCevabi);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public ViewResult CevaplariListele()
+        {
+            return View(Repository.Cevaplar.Where(x => x.DuguneGelecekmi == true));
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Dugun_Davet/Models/Cevap.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dugun_Davet.Models
+{
+    public class Cevap
+    {
+        [Required(ErrorMessage ="Lütfen ad alanını boş bırakmayınız!")]
+        public string Ad { get; set; }
+        [Required(ErrorMessage ="Lütfen soyad alanını boş bırakmayınız!")]
+        public string Soyad { get; set; }
+        [Required(ErrorMessage = "Lütfen email alanını boş bırakmayınız!")]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Lütfen telefon alanını boş bırakmayınız!")]
+        public string Telefon { get; set; }
+        [Required(ErrorMessage = "Lütfen bir seçim yapınız!")]
+        public bool? DuguneGelecekmi { get; set; }
+    }
+}
